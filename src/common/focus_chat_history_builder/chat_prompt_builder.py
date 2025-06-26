4c1c8a37bdc61e53b7c6e9e0ac7d0eb877c5d6b7
114514 <2514624910@qq.com>
2025-06-26 22:38:39 +0800

    删除多余导入

--- src/common/focus_chat_history_builder/chat_prompt_builder.py
+++ src/common/focus_chat_history_builder/chat_prompt_builder.py
@@ -12,8 +12,6 @@ from aicarus_protocols.conversation_info import ConversationInfo
 from aicarus_protocols.event import Event
 from aicarus_protocols.seg import Seg
 from aicarus_protocols.user_info import UserInfo
-from aicarus_protocols.conversation_info import ConversationInfo
-from aicarus_protocols.common import extract_text_from_content
 from src.common.custom_logging.logging_config import get_logger
 from src.config import config
 
